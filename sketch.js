1d1a25b3eed23274471448cb8581309e2f843578
innapau <jemmek364@gmail.com>
2020-06-15 18:58:35 +0300

    edit sketch.js

--- sketch.js
+++ sketch.js
@@ -91,23 +91,35 @@ function resetup() {
 let firstMousePress = true;
 
 function mousePressed() {
-    stroke(0);
-    noFill();
-    rect(this.x, this.y, this.w, this.w);
-    if (this.revealed) {
-        if (this.mine) {
-            fill(127);
-            ellipse(this.x + this.w * 0.5, this.y + this.w * 0.5, this.w * 0.5);
-        } else {
-                fill(200);
-                rect(this.x, this.y, this.w, this.w);
-            if (this.neighborCount > 0) {
-                textAlign(CENTER);
-                fill(0);
-                text(this.neighborCount, this.x + this.w * 0.5, this.y + this.w - 6);
+    background(255);
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            if(grid[i][j].contains(mouseX, mouseY) && !grid[i][j].marked) {
+                grid[i][j].reveal();
+                if (grid[i][j].mine) {
+                    if (firstMousePress) {
+                        grid[i][j].mine = false;
+
+                        // Выбираем мину на замену
+                        let newMine = undefined;
+                        while (!newMine) {
+                            let c = random(cols);
+                            let r = random(rows);
+
+                            if (!grid[c][r].mine) {
+                                grid[c][r].mine = true;
+                                newBee = grid[c][r];
+                            }
+                        }
+                    } else {
+                        noLoop();
+                        gameOver();
+                    }
+                }
             }
         }
     }
+    firstMousePress = false;
 };
 
 function keyReleased() {
